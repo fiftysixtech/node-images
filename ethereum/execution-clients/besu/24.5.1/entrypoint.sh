38f133146ee22baad7d0b4168095f3a333e4e6bb
Daniel Peter <curveballdaniel@gmail.com>
2024-05-17 00:12:29 -0400

    Change besu to work off command rather than direct path

--- ethereum/execution-clients/besu/24.5.1/entrypoint.sh
+++ ethereum/execution-clients/besu/24.5.1/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+# add besu script to PATH
+mkdir -p /etc/besu
+ln -sf /node/besu/data/bin/besu /etc/besu/besu
+export PATH="/etc/besu:$PATH"
+
 # generate a new jwt file
 openssl rand -hex 32 | tr -d "\n" > "/${ROOT_DIR}/data/jwt.hex"
 
 chown -R nodeuser "${ROOT_DIR}"
 
-if [ "$1" = "data/bin/besu" ]; then
+if [ "$#" -eq 1 ] && [ "$1" = "besu" ]; then
   set -- "$1" --config-file="${ROOT_DIR}/configs/config.toml" "${@:2}"
 fi
 
-if [ "$1" = "data/bin/besu" ]; then
+if [ "$1" = "besu" ]; then
   exec gosu nodeuser "$@"
 else
   exec "$@"
