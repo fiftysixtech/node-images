d95d7e8dd63e1a9275a334ae278458839e20772a
Daniel Peter <curveballdaniel@gmail.com>
2024-08-08 13:07:32 -0400

    Add ord-litecoin version 0.19.1

---
+++ litecoin/ord-litecoin/0.19.1/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+chown -R nodeuser "${ROOT_DIR}"
+
+if [ "$(echo "$1" | cut -c1)" = "-" ]; then
+  set -- ord "$@"
+fi
+
+if [ "$1" = "ord" ]; then
+  set -- "$1" --config "${ROOT_DIR}/configs/ord-litecoin.yaml" "${@:2}"
+fi
+
+if [ "$1" = "ord" ]; then
+  exec gosu nodeuser "$@"
+else
+  exec "$@"
+fi
